56717c84e990b006c9093fcbeeeeb7b1632bb042
Mohiuddin Sumon <msumon2048@gmail.com>
2022-10-07 16:07:50 +0600

    added a new cart component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Cart = ({cart}) => {
+    return (
+        <div>
+            <h2>This is order summery</h2>
+            <p>Selected Items: {cart.length}</p>
+
+        </div>
+    );
+};
+
+export default Cart;
